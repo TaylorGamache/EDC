c8b89176a569a6eb51559c73405d40c4621ee29e
TaylorGamache <taylor.gamache@uconn.edu>
2016-04-01 13:40:07 -0400

    Added a delete function and started editing the swagger page and endpoint for adding new Email Digest Actions.

--- app.js
+++ app.js
@@ -44,44 +44,86 @@ db.index(function(er, result) {
 });
 */
 
-//Gets all Emails out of the database and puts them in a queue at startup
-var allDocs = {"selector": { "_id": { "$gt": 0}}};
-db.find(allDocs ,function(err, result){
-	if (err) {
-		throw err;
-	} 
-	console.log('Found %d Email JSONs at startup.', result.docs.length);
-    for (var i = 0; i < result.docs.length; i++) {
-		console.log('Email Number: %d', (i+1));
-		console.log('Email Subject: %s', result.docs[i].Subject);
-		console.log('Email Body: %s', result.docs[i].Body);
-		Q.push(result.docs[i]);
-    }
-});
-
-//Endpoint for adding emails to database and queue
-app.post('/api/v1/emailDigest/queue', function(req, res){
-	var request = req.body;
+/*For sending out emails
+		//Gets all Emails out of the database and puts them in a queue at startup
+		var allDocs = {"selector": { "_id": { "$gt": 0}}};
+		db.find(allDocs ,function(err, result){
+			if (err) {
+				throw err;
+			} 
+			console.log('Found %d Email JSONs at startup.', result.docs.length);
+			for (var i = 0; i < result.docs.length; i++) {
+				console.log('Email Number: %d', (i+1));
+				console.log('Email Subject: %s', result.docs[i].Subject);
+				console.log('Email Body: %s', result.docs[i].Body);
+				Q.push(result.docs[i]);
+			}
+		});*/
+		
+app.delete('/api/v1/emailDigest/:recipeid', function(req, res){
+	var del_ID = req.params.recipeid;
 	
+	db.get(del_ID, function(err, data){
+		if(err){
+			res.json({success: false, msg: 'Failed to find the Email Digest Action in the database, please try again.'});
+		} else {
+			var rev = data._rev;
+			db.destroy(del_ID, rev,  function(err) {
+				if (!err) {
+					res.json({success: true, msg: 'Successfully deleted the Email Digest Action from the database.'});
+					console.log("Successfully deleted doc"+ del_ID);
+				} else {
+					res.json({success: false, msg: 'Failed to delete the Email Digest Action from the database, please try again.'});
+					//console.log("failed");
+				}
+			});
+		}
+	});
+});	
+
+//Endpoint for adding emails to database
+app.post('/api/v1/emailDigest/new', function(req, res){
+	var request = req.body;
+	request.timer.callbackURL = "";
+	//request.timer.recipeID = "";
+	
+	//Makes sure data in JSON is formatted and submitted correctly.
 	if(request.To == "") {
-		res.json({success: false, msg: 'No emails to send to were submitted.'});
-	} else if (request.Subject == "") {
-		res.json({success: false, msg: 'No subject was submitted.'});
-	} else if (request.Body == "") {
-		res.json({success: false, msg: 'No email body was submitted.'});
+		res.json({success: false, msg: 'No email to send to were submitted.'});
 	} else if (request.callback == "") {
 		res.json({success: false, msg: 'No callback was submitted.'});
+	} else if (request.timer.trigger.timezone == "") {
+		res.json({success: false, msg: 'No timezone was submitted.'});
+	} else if (request.timer.trigger.hour > 23 || request.time.hour < 0) {
+		res.json({success: false, msg: 'Incorrect formatted hour was submitted.'});
+	} else if (request.timer.trigger.min > 59 || request.time.min < 0) {
+		res.json({success: false, msg: 'Incorrect formatted min was submitted.'});
+	} else if (request.timer.trigger.days >6 || request.time.days < 0) {
+		res.json({success: false, msg: 'Incorrect formatted days was submitted.'});
+	} else if (request.timer.trigger.date > 31 || request.time.date < 0) {
+		res.json({success: false, msg: 'Incorrect formatted date was submitted.'});
+	} else if (request.timer.trigger.month > 12 || request.time.month < 1) {
+		res.json({success: false, msg: 'Incorrect formatted month was submitted.'});
 	} else {
-
+		
+		//Insert new Email Digest Action into database
 		db.insert(request, function(err, body, header){
 			if(err){
-				res.json({success:false, msg:'Error adding Email action.'});
+				res.json({success:false, msg:'Error adding Email Digest action.'});
 			}else{
 				var idNum = body.id;
 				res.json({success: true, msg: 'Successfully added the Email action to database.'});
 				
-				//Add email to queue
-				Q.push(request);
+				console.log('New Email Added to Database');
+				//Add a timer
+				request(requestURL, function(err, response, body){
+					if(!err){
+						console.log("successful response from Timer API!");
+						var parsedbody = JSON.parse(body);
+						var changePercent = parsedbody.ChangePercent;
+					}
+				});
+				
 			}
 		});
 	}
