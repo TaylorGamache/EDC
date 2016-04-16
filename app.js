f178887fdb1a5c19c23baa140c9394f8a1671661
TaylorGamache <taylor.gamache@uconn.edu>
2016-04-16 00:28:04 -0400

    Added a way to send emails for demo and updated the code and swagger page.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var request = require('request');
 var Cloudant = require('cloudant');
 var Config = require('config-js');
+var sendGrid = require('sendgrid')('YOUR_SENDGRID_API_KEY');
 var json = require('json');
 var config = new Config('./email_digest_config.js');
 var me = config.get('CLOUDANT_USERNAME');
@@ -30,21 +31,13 @@ var Q = [];
 app.use(bodyParser.json());
 // app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-/*
-//Used to find all indexes of db
-db.index(function(er, result) {
-  if (er) {
-    throw er;
-  }
 
-  console.log('The database has %d indexes', result.indexes.length);
-  for (var i = 0; i < result.indexes.length; i++) {
-    console.log('  %s (%s): %j', result.indexes[i].name, result.indexes[i].type, result.indexes[i].def);
-  }
-});
-*/
+/******
 
-//For sending out emails
+INIT
+
+******/
+
 //Gets all Emails out of the database and puts them in a queue at startup
 var allDocs = {"selector": { "_id": { "$gt": 0}}};
 db.find(allDocs ,function(err, result){
@@ -56,34 +49,82 @@ db.find(allDocs ,function(err, result){
 		//console.log('Email Number: %d', (i+1));
 		//console.log('Email Subject: %s', result.docs[i].Subject);
 		//console.log('Email Body: %s', result.docs[i].Body);
-		Q.push(result.docs[i]);
+		var email = {'to':result.docs[i].To, 'from': 'Email@Digest.Test', 
+					'subject': result.docs[i].Subject,'text':result.docs[i].Body};
+		var eQue = {'id':result.docs[i]._id, 'Timer':result.docs[i].timer, 'Email':email  };
+		Q.push(eQue);
+		
 	}
 	console.log('Email Digest Channel is up and running.');
 });
-		
-app.delete('/api/v1/emailDigest/:recipeid', function(req, res){
-	var del_ID = req.params.recipeid;
-	
-	db.get(del_ID, function(err, data){
-		if(err){
-			res.json({success: false, msg: 'Failed to find the Email Digest Action in the database, please try again.'});
-		} else {
-			var rev = data._rev;
-			db.destroy(del_ID, rev,  function(err) {
-				if (!err) {
-					res.json({success: true, msg: 'Successfully deleted the Email Digest Action from the database.'});
-					console.log("Successfully deleted doc"+ del_ID);
-				} else {
-					res.json({success: false, msg: 'Failed to delete the Email Digest Action from the database, please try again.'});
-					//console.log("failed");
-				}
-			});
-		}
-	});
-});	
 
-//Endpoint for adding emails to database
-app.post('/api/v1/emailDigest/new', function(req, res){
+/***********************************
+
+ADD NEW OR UPDATE A RECIPE IN QUEUE
+
+***********************************/
+		
+//Endpoint for adding or changing emails in queue
+app.post('/api/v1/emailDigest/:recipeid, msg, subject, destination, aggregation', function(req, res){
+	var ID = req.params.recipeid;
+	var to = req.params.destination;
+	var msg = req.params.msg;
+	var sub = req.params.subject;
+	var aggr = req.params.aggregation;
+	request.timer.callbackURL = "";
+	//request.timer.recipeID = "";
+	
+	//Makes sure data in JSON is formatted and submitted correctly.
+	if(to == "") {
+		res.json({success: false, msg: 'No email to send to were submitted.'});
+	} else if (ID == "") {
+		res.json({success: false, msg: 'No recipeID was submitted.'});
+	} else if (request.timer == "day" || request.timer == "week" || request.timer == "month") {
+		res.json({success: false, msg: 'Incorrect timer type was submitted.'});
+	}  else {
+		res.json({success: true, msg: 'Email was submitted.'});
+		//Check if the ID exists and if it does update it
+		var tempQ = [];
+		var check = true;
+		var exists = false; 
+		while (check) {
+			//console.log(item);
+			if (Q == [] ) {
+				check = false;
+			} else  {
+				var item = Q.pop();
+				if (item.id == ID) {
+					exists = true;
+					item.Timer = aggr;
+					item.Email.to = to;
+					item.Email.subject = sub;
+					item.Email.text = msg;
+				}
+				tempQ.push(item);
+				
+			}
+		}
+		//reset Q
+		Q = tempQ;
+		// If ID was not in queue than add it
+		if (exists == false) {
+			var email = {'to':to, 'from': 'Email@Digest.Test', 
+						'subject': sub,'text':msg};
+
+			var eQue = {'id':ID, 'Timer':aggr, 'Email':email  };
+			Q.push(eQue);
+		} 		
+	}
+});
+
+/******************
+
+Temperary Endpoints
+
+******************/
+
+//Endpoint for adding emails to database (Temperary)
+app.post('/api/v1/temp/new', function(req, res){
 	var request = req.body;
 	request.timer.callbackURL = "";
 	//request.timer.recipeID = "";
@@ -96,28 +137,6 @@ app.post('/api/v1/emailDigest/new', function(req, res){
 	} else if (request.timer == "day" || request.timer == "week" || request.timer == "month") {
 		res.json({success: false, msg: 'Incorrect timer type was submitted.'});
 	}  else {
-		//Start setting up the Timer recipe
-		var trig = {
-			"recipeID": "string",
-			"callbackURL": "string",
-			"trigger": {
-				"repeat": 0,
-				"hour": 23,
-				"min": 59,
-				"interval": 0,
-				"days": 0,
-				"date": 1,
-				"month": 1,
-				"timezone": "America/New_York"
-			}
-		};
-		if (request.timer == "day") {
-			
-		} else if (request.timer == "week") {
-			
-		} else {
-			
-		}
 		
 		//Insert new Email Digest Action into database
 		db.insert(request, function(err, body, header){
@@ -128,48 +147,44 @@ app.post('/api/v1/emailDigest/new', function(req, res){
 				res.json({success: true, msg: 'Successfully added the Email action to database.'});
 				
 				console.log('New Email Added to Database');
-				//Add a timer
-				//Have not actually set up yet
-				request(requestURL, function(err, response, body){
-					if(!err){
-						console.log("successful response from Timer API!");
-						var parsedbody = JSON.parse(body);
-					}
-				});
+				var email = {'to':request.To, 'from': 'Email@Digest.Test', 
+							'subject': request.Subject,'text':request.Body};
 				
+				var eQue = {'id':request._id, 'Timer':request.timer, 'Email':email  };
+				Q.push(eQue);			
 			}
 		});
 	}
 });
 
-//I think we need another endpoint for sending emails but they say we do not
- 
-//Function for sending email in queue
-//I am not really sure how it works yet, I copied and pasted from what they gave us
-exports.send = function(from, to, subject, body, callback) {
+//Sends email
+
+app.post('/api/v1/temp/send/:recipeid', function(req, res){
+	var ID = req.params.recipeid;
+	var e;
+	var check = true;
+	var tempQ = Q;
+	while (check) {
+			if (tempQ == [] ) {
+				check = false;
+				res.json({success: false, msg: 'Failed to find matching recipeID.'});
+			} else {
+				var item = tempQ.pop();
+				if (item.id == ID) {
+					check = false;
+					e = item.Email;
+				}
+			}
+		}
 	
-  var formData = {
-    sendTo: to,
-    from: from,
-    subject: subject,
-    html: body,
-    applicationKey: settings.connect2mail.appKey
-  };
-
-  var jsonData = {
-    method: 'POST',
-    headers: {},
-    url: settings.connect2mail.endpoint,
-    data: formData
-  };
-
-  httpQueue.create(jsonData, function() {
-    callback(null, true);
-    return;
-  });
-
-};
-
+	sendGrid.send(e, function(err, json) {
+	if (err) { 
+		res.json({success: false, msg: 'Failed to send Email.'});
+	}
+		res.json({success: true, msg: 'Successfully sent email.'});
+	});
+	
+});
 
 
 
